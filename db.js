223e52f875fd2c3dfcb4d5e6303fba6918848de0
Max Kayen <mkayen@gmail.com>
2015-09-02 22:09:51 -0400

    added db cred

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var DATABASE_URI = // You should put DB information here.
+var DATABASE_URI = "mongodb://mkayen:Nov91990@ds031832.mongolab.com:31832/heroku_138rsq9k"
 var db = mongoose.connect(DATABASE_URI).connection;
 
 module.exports = db;
